1ac317e73105e97d5c6aa28d68ea78224af3c1b1
Leonardo <martinezjleonardo@gmail.com>
2024-09-10 21:25:09 -0300

    agregando weas

--- control.gd
+++ control.gd
@@ -4,11 +4,15 @@ extends Control
 @onready var montoNode = $Panel/BoxContainer/VBoxContainer/Monto
 var monto: int = 0
 @onready var descripcion =$Panel/BoxContainer/VBoxContainer3/Descripcion
+@onready var POST = $HTTPRequest
 var itemSelected = 0
+var url = "https://api-godot.vercel.app/data"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$OK.visible = false
+	$Panel.visible = true
+	$WRONG.visible = false
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -39,6 +43,19 @@ func _on_list_items_item_selected(index: int) -> void:
 
 func sendData(itemSelected, monto, descripcion):
 	if itemSelected != "" and monto > 0 and descripcion != "":
-		print("ALL OK")
+		var jsonData = JSON.stringify({"rubro":itemSelected,"monto":monto,"descripcion":descripcion})
+		var headers = ["Content-Type: application/json"]
+		POST.request(url, headers, HTTPClient.METHOD_POST, jsonData)
+		$OK.visible = true
+		$Panel.visible = false
+		$WRONG.visible = false
 	else:
-		print("falta data")
+		$OK.visible = false
+		$Panel.visible = false
+		$WRONG.visible = true
+
+
+func _on_volver_pressed() -> void:
+	$OK.visible = false
+	$Panel.visible = true
+	$WRONG.visible = false
